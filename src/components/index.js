20abf2d402079caa1953c94a58574c1f2bdfe7e6
Dev Garg <gargdev010300@gmail.com>
2025-04-11 10:48:01 +0530

    auth components added

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,10 @@ import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Input from "./Input";
 import Button from "./Button";
+import Select from "./Select";
+import PostCard from "./PostCard";
+import Login from "./Login";
+import Signup from "./Signup";
+import Protected from "./AuthLayout";
 
-export { Header, Footer, Container, Logo, LogoutBtn, Input, Button };
+export { Header, Footer, Container, Logo, LogoutBtn, Input, Button, Select, PostCard, Login, Signup, Protected };
