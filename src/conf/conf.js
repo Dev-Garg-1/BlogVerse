9f8dcd750dcb2431eb01f68b45a58efcf46c4bbe
Dev Garg <gargdev010300@gmail.com>
2025-04-15 14:51:21 +0530

    configured tinymce api key

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -3,9 +3,8 @@ const conf = {
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID)
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    tinyMceApiKey: String(import.meta.env.VITE_TINY_MCE_API_KEY),
 }
-
-console.log("Appwrite Config:", conf); 
 
 export default conf;
